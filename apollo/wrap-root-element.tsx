c19a16f6ca788b1a50854822a071a42075e0c374
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2022-09-26 19:51:31 -0400

    Update wrap-root-element.tsx

--- apollo/wrap-root-element.tsx
+++ apollo/wrap-root-element.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { client } from './apollo-client.ts';
+import { client } from './apollo-client';
 
 
 export const WrapRootElement = ({ element }) => (
