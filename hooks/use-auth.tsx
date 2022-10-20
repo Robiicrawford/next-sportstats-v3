9ef10183cb73764f3f40a3c4f5bddb86141ebc05
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2022-10-20 14:48:20 -0400

    Update use-auth.tsx

--- hooks/use-auth.tsx
+++ hooks/use-auth.tsx
@@ -14,7 +14,7 @@ export function ProvideAuth({ children }) {
 
 // Hook for child components to get the auth object ...
 // ... and re-render when it changes.
-export const useAuth = () => {
+export const useAuth = () :any => {
   return useContext(authContext);
 };
 
