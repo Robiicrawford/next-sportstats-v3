c46c69c8764885af98545b14b23ab0a73fb569b1
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2022-10-21 08:57:36 -0400

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -10,6 +10,10 @@ import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false; /* eslint-disable import/first */
 
+type Layout = {
+    header_color?: String
+}
+
 
 const Layout = ({ children, header_color }) => {
  
