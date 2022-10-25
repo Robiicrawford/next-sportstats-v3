a168d4a509bef7f9e55d1b327385d6faec3c6745
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2022-10-25 10:03:08 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -72,15 +72,17 @@ function Application({ Component, pageProps }: AppProps) {
             }}
           />
           <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6314308690124297" />
-           <Script id="google-analytics" strategy="afterInteractive">
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
             {`
-              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-              })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-              ga('create', 'G-PRXLRELM2K', 'auto');
-              ga('send', 'pageview');
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
             `}
           </Script>
           
