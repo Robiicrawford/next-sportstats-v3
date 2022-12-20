2d594f5f742a227a6209c086bf7b97be35845233
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2022-12-20 18:53:01 -0500

    move office locations to json array

--- pages/help/offices.tsx
+++ pages/help/offices.tsx
@@ -8,7 +8,8 @@ import Link from 'next/link'
 
 import { 
   Box, Flex, Heading, Text, Center, SimpleGrid,
-  Card, CardBody, CardFooter, Stack, Image, Badge 
+  Card, CardBody, CardFooter, Stack, Image, Badge,
+  useColorModeValue
 } from '@chakra-ui/react';
 
 import Layout from '../../components/layout/Layout'
@@ -28,7 +29,51 @@ const IndexPage = ({ data }) => {
 
   const {t} = useTranslation('public');
 
- 
+  
+  const officeList = [
+    { hq: true, name:'Sportstats East', 
+      image: 'https://a.cdn-hotels.com/gdcs/production163/d857/cc29dd0e-f745-4507-80e1-6ae5a3532338.jpg',
+      addressLine1: "155 Colonnade Rd. #18",
+      addressLine2: "Ottawa, ON K2E 7K1 (Canada)",
+      addressLine3: "",
+      countryCode:'CA'
+    },
+    { hq: false, name:'Sportstats West', 
+      image: 'https://d3exkutavo4sli.cloudfront.net/wp-content/uploads/2019/04/the_heights_on_austin_rendering_1.jpg',
+      addressLine1: "118 - 250 Schoolhouse St",
+      addressLine2: "Coquitlam, BC, V3K 6V7",
+      addressLine3: "",
+      countryCode:'CA'
+    },
+    { hq: false, name:'Sportstats USA', 
+      image: 'https://www.mercyhealth.com/assets/images/medical-education/grand-rapids/grand-rapids-banner.jpg',
+      addressLine1: "88 54th St. SW Suite 106",
+      addressLine2: "Grand Rapids, MI 49548",
+      addressLine3: "",
+      countryCode:'US'
+    },
+    { hq: false, name:'Sportstats Quebec', 
+      image: 'https://i0.wp.com/thevalemagazine.com/wp-content/uploads/2019/03/fairmont-le-ch%C3%A2teau-frontenac-que%CC%81bec-city-canada.jpg?fit=1200%2C800&ssl=1',
+      addressLine1: "3075 Boul. Hamel, Bureau 109",
+      addressLine2: "Quebec, QC, G1P 4C6",
+      addressLine3: "",
+      countryCode:'CA'
+    },
+    { hq: false, name:'Sportstats Asia', 
+      image: 'https://www.trailsofindochina.com/wp-content/uploads/2018/04/Bangkok_header.jpg',
+      addressLine1: "109 CCT Bldg 8th Floor,",
+      addressLine2: "Surawong Rd, Bangrak",
+      addressLine3: "",
+      countryCode:'TH'
+    },
+    { hq: false, name:'Sportstats India', 
+      image: 'https://static.toiimg.com/photo/50913699.cms',
+      addressLine1: "Brigade Gateway,",
+      addressLine2: "26/1 Dr. Rajkumar Road,",
+      addressLine3: "Bangalore, 560055",
+      countryCode:'IN'
+    }
+  ]
 
   return (
     <Layout>
@@ -39,13 +84,11 @@ const IndexPage = ({ data }) => {
       </Center >
       <Section.Container id="settings"  Background={Background} >
       
-      
           <Flex 
             flexWrap='wrap' justifyContent='center' 
             gap='2em' pb='150px'  px={[1,3,5]} pt={2}
           >
             <SideMenu active='offices' />
-
 
             <Flex
               w={['100%','100%','60%']}
@@ -54,235 +97,55 @@ const IndexPage = ({ data }) => {
             >
               <Heading mb='4' pb='2' sx={{borderBottom:'3px solid black'}}>  Office Locations </Heading>
               <SimpleGrid columns={[1]} spacing={10} pb='5'>
-                <Card
-                  direction={{ base: 'column', sm: 'column', md:'row' }}
-                  overflow='hidden'
-                  variant='outline'
-                >
-                  <Image
-                    objectFit='cover'
-                    maxW={{ base: '100%', sm: '220px' }}
-                    maxH={{ base: '100%', sm: '100%' }}
-                    style={{objectFit:'contain'}}
-                    src='https://a.cdn-hotels.com/gdcs/production163/d857/cc29dd0e-f745-4507-80e1-6ae5a3532338.jpg'
-                    alt='Caffe Latte'
-                  />
+                {officeList.map((office)=>
+                  <Card
+                    direction={{ base: 'column', sm: 'column', md:'row' }}
+                    overflow='hidden'
+                    variant='outline'
+                    bg={useColorModeValue('white', 'gray.700')}
+                    shadow={{ md: 'base' }}
+                  >
+                    <Image
+                      objectFit='cover'
+                      maxW={{ base: '100%', sm: '220px' }}
+                      maxH={{ base: '100%', sm: '100%' }}
+                      style={{objectFit:'contain'}}
+                      src={office.image}
+                      alt={office.name}
+                    />
 
-                  <Stack>
-                    <CardBody>
-                      <Heading size='md'>
-                        <ReactCountryFlag 
-                          svg 
-                          countryCode='CA'
-                          title='Canada'
-                          style={{marginRight:'0.6em'}}
-                        /> 
+                    <Stack>
+                      <CardBody>
+                        <Heading size='md'>
+                          <ReactCountryFlag 
+                            svg 
+                            countryCode={office.countryCode}
+                            title={office.countryCode}
+                            style={{marginRight:'0.6em'}}
+                          /> 
 
-                        Sportstats East
-                        
-                        <Badge ml='1' colorScheme='green'>
-                          HQ
-                        </Badge>
+                          {office.name}
+                          {office.hq === true &&
+                            <Badge ml='1' colorScheme='green'>
+                              HQ
+                            </Badge>
+                          }
+                        </Heading>
 
-
-                      </Heading>
-
-                      <Text py='2'>
-                        155 Colonnade Rd. #18 <br/>
-                        Ottawa, ON K2E 7K1 (Canada)
-                      </Text>
-                    </CardBody>
-                  </Stack>
-                </Card>
-                
-                <Card
-                  direction={{ base: 'column', sm: 'column', md:'row' }}
-                  overflow='hidden'
-                  variant='outline'
-                >
-                  <Image
-                    objectFit='cover'
-                    maxW={{ base: '100%', sm: '230px' }}
-                    maxH={{ base: '100%', sm: '100%' }}
-                    src='https://d3exkutavo4sli.cloudfront.net/wp-content/uploads/2019/04/the_heights_on_austin_rendering_1.jpg'
-                    alt='Caffe Latte'
-                  />
-
-                  <Stack>
-                    <CardBody>
-                      <Heading size='md'>
-                        <ReactCountryFlag 
-                          svg 
-                          countryCode='CA'
-                          title='Canada'
-                          style={{marginRight:'0.6em'}}
-                        /> 
-
-                        Sportstats West
-
-                      </Heading>
-
-                      <Text py='2'>
-                        118 - 250 Schoolhouse St, <br/>
-                        Coquitlam, BC, V3K 6V7
-                      </Text>
-                    </CardBody>
-                  </Stack>
-                </Card>
-
-                <Card
-                  direction={{ base: 'column', sm: 'column', md:'row' }}
-                  overflow='hidden'
-                  variant='outline'
-                >
-                  <Image
-                    objectFit='cover'
-                    maxW={{ base: '100%', sm: '230px' }}
-                    maxH={{ base: '100%', sm: '100%' }}
-                    src='https://www.mercyhealth.com/assets/images/medical-education/grand-rapids/grand-rapids-banner.jpg'
-                    alt='Caffe Latte'
-                  />
-
-                  <Stack>
-                    <CardBody>
-                      <Heading size='md'>
-                        <ReactCountryFlag 
-                          svg 
-                          countryCode='US'
-                          title='USA'
-                          style={{marginRight:'0.6em'}}
-                        /> 
-
-                        Sportstats USA
-
-                        
-                      </Heading>
-
-                      <Text py='2'>
-                        88 54th St. SW Suite 106 <br/>
-                        Grand Rapids, MI 49548
-                      </Text>
-                    </CardBody>
-                  </Stack>
-                </Card>
-
-                 <Card
-                  direction={{ base: 'column', sm: 'column', md:'row' }}
-                  overflow='hidden'
-                  variant='outline'
-                >
-                  <Image
-                    objectFit='cover'
-                    maxW={{ base: '100%', sm: '230px' }}
-                    maxH={{ base: '100%', sm: '100%' }}
-                    src='https://i0.wp.com/thevalemagazine.com/wp-content/uploads/2019/03/fairmont-le-ch%C3%A2teau-frontenac-que%CC%81bec-city-canada.jpg?fit=1200%2C800&ssl=1'
-                    alt='Caffe Latte'
-                  />
-
-                  <Stack>
-                    <CardBody>
-                      <Heading size='md'>
-                        <ReactCountryFlag 
-                          svg 
-                          countryCode='CA'
-                          title='Canada'
-                          style={{marginRight:'0.6em'}}
-                        /> 
-
-                        Sportstats Quebec
-
-                      </Heading>
-
-                      <Text py='2'>
-                        3075 Boul. Hamel, Bureau 109 <br/>
-                        Quebec, QC, G1P 4C6
-                      </Text>
-                    </CardBody>
-                  </Stack>
-                </Card>
-
-
-                <Card
-                  direction={{ base: 'column', sm: 'column', md:'row' }}
-                  overflow='hidden'
-                  variant='outline'
-                >
-                  <Image
-                    objectFit='cover'
-                    maxW={{ base: '100%', sm: '230px' }}
-                    maxH={{ base: '100%', sm: '100%' }}
-                    src='https://www.trailsofindochina.com/wp-content/uploads/2018/04/Bangkok_header.jpg'
-                    alt='Caffe Latte'
-                  />
-
-                  <Stack>
-                    <CardBody>
-                      <Heading size='md'>
-                        <ReactCountryFlag 
-                          svg 
-                          countryCode='TH'
-                          title='Thailand'
-                          style={{marginRight:'0.6em'}}
-                        /> 
-
-                        Sportstats Asia
-
-                      </Heading>
-
-                      <Text py='2'>
-                        109 CCT Bldg 8th Floor, <br/>
-                        Surawong Rd, Bangrak
-                      </Text>
-                    </CardBody>
-                  </Stack>
-                </Card>
-
-                <Card
-                  direction={{ base: 'column', sm: 'column', md:'row' }}
-                  overflow='hidden'
-                  variant='outline'
-                >
-                  <Image
-                    objectFit='cover'
-                    maxW={{ base: '100%', sm: '230px' }}
-                    maxH={{ base: '100%', sm: '100%' }}
-                    src='https://static.toiimg.com/photo/50913699.cms'
-                    alt='Caffe Latte'
-                  />
-
-                  <Stack>
-                    <CardBody>
-                      <Heading size='md'>
-                        <ReactCountryFlag 
-                          svg 
-                          countryCode='IN'
-                          title='India'
-                          style={{marginRight:'0.6em'}}
-                        /> 
-
-                        Sportstats India 
-
-                      </Heading>
-
-                      <Text py='2'>
-                        Brigade Gateway,<br/>
-                        26/1 Dr. Rajkumar Road, <br/>
-                        Bangalore, 560055
-                      </Text>
-                    </CardBody>
-                  </Stack>
-                </Card>
+                        <Text py='2'>
+                          {office.addressLine1} <br/> {office.addressLine2}
+                        </Text>
+                      </CardBody>
+                    </Stack>
+                  </Card>
+                )}
 
               </SimpleGrid>
             </Flex>
           </Flex>
-          
-       
-
-
       </Section.Container>
       
-       <ContactUsBanner />
+      <ContactUsBanner />
 
     </Layout>
   )
