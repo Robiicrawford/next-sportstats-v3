33cf545d77021c89fee9cc6d5e9efd3859448c35
Robiicrawford <50127771+Robiicrawford@users.noreply.github.com>
2023-01-10 14:44:10 -0500

    d

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -70,10 +70,9 @@ function Application({ Component, pageProps }: AppProps) {
           <Script 
             id="Adsense-id"
             data-ad-client="ca-pub-631430869012429"
-            async strategy="afterInteractive"
+            strategy="afterInteractive"
             onError={ (e) => { console.error('Script failed to load', e) }}
-            data-adtest="on"
-            async="true"
+            async={true}
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6314308690124297" 
           />
           <Script
